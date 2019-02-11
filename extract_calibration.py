03736fee1c8040cc6ed6fe1d7120c925c86aa0b6
Anirudh <anijaya9@gmail.com>
2019-02-11 14:24:38 +0100

    Update extract_calibration.py

--- extract_calibration.py
+++ extract_calibration.py
@@ -3,7 +3,7 @@ import cv2
 # FILE_STORAGE_READ
 cv_file = cv2.FileStorage("calib_images/test.yaml", cv2.FILE_STORAGE_READ)
 
-#note we also have to specify the type to retrieve other wise we only get a
+# Note : we also have to specify the type to retrieve otherwise we only get a
 # FileNode object back instead of a matrix
 camera_matrix = cv_file.getNode("camera_matrix").mat()
 dist_matrix = cv_file.getNode("dist_coeff").mat()
