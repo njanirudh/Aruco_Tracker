c0406e172ed23858613c20d1e70fe257af46aa27
Anirudh NJ <anijaya9@gmail.com>
2020-12-07 09:01:33 +0100

    Update pose_estimation.py

--- pose_estimation.py
+++ pose_estimation.py
@@ -45,10 +45,6 @@ def draw(img, corners, imgpts):
     img = cv2.line(img, corner, tuple(imgpts[2].ravel()), (0, 0, 255), 5)
     return img
 
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-
 axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
 
 for fname in glob.glob('calib_images/checkerboard/left03.jpg'):
